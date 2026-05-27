7247048d37fac6e74c6468cfe0585a35f1bf8a47
star.rajendran <star.rajendran@gmail.com>
2026-05-27 15:19:45 +0530

    simple Node App for Jenkins

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js App!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
